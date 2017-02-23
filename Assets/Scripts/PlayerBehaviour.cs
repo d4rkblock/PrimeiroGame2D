6c285c17e073136ecba58a425bf4ad5a169bdb0d
User <User>
2017-02-22 21:53:32 -0300

    primeira aula

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public float speed = 10;
+
+	private Rigidbody2D rb2d;
+
+	void Awake()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+		rb2d.AddForce(movement);
+	}
+
+	void OnTriggerEnter2D(Collider2D collisor){
+		if (collisor.gameObject.CompareTag ("Ouro")) {
+			Destroy (collisor.gameObject);
+		}
+	}
+}
